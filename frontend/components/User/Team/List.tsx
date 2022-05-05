7a4e1de27f2e7bbe1e9d83f9fec79bc44b2d6c1f
natestormer <nate@smartrent.com>
2022-05-05 09:47:04 -0700

    adds teams list and team page

--- frontend/components/User/Team/List.tsx
+++ frontend/components/User/Team/List.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link"
 import { ReactNode } from "react"
+import { useUser } from "../../../hooks"
 
 interface UserTeamListProps {
   list?:
@@ -16,6 +18,7 @@ const UserTeamList = ({
   list,
   emptyMessage = "No items found",
 }: UserTeamListProps) => {
+  const { user } = useUser()
   if (!list) return <p>{emptyMessage}</p>
 
   return (
@@ -25,22 +28,25 @@ const UserTeamList = ({
         {list.map((item) => {
           if (item) {
             return (
-              <li
-                key={item.id}
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  borderBottom: "1px solid gray",
-                }}
-              >
-                <h4 style={{ flex: "1 0 25%" }}>{item.name}</h4>
-                <div style={{ flex: "1 0 25%" }}>
-                  {item.currentUserIsAuthor && <span>Author</span>}
-                </div>
-                <div style={{ flex: "1 0 25%" }}>
-                  {item.currentUserIsManager && <span>Manager</span>}
-                </div>
+              <li key={item.id}>
+                <Link href={`/user/${user?.id}/teams/${item.id}`} passHref>
+                  <a
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      borderBottom: "1px solid gray",
+                    }}
+                  >
+                    <h4 style={{ flex: "1 0 25%" }}>{item.name}</h4>
+                    <div style={{ flex: "1 0 25%" }}>
+                      {item.currentUserIsAuthor && <span>Author</span>}
+                    </div>
+                    <div style={{ flex: "1 0 25%" }}>
+                      {item.currentUserIsManager && <span>Manager</span>}
+                    </div>
+                  </a>
+                </Link>
               </li>
             )
           }
---
+++ frontend/pages/user/[id]/teams/[teamId].tsx
@@ -0,0 +1,71 @@
+import type { NextPage, GetServerSideProps } from "next"
+
+import { addApolloState, initializeApollo } from "../../../../lib/apolloClient"
+import {
+  UserTeamsTeamPageDocument,
+  UserTeamsTeamPageQuery,
+} from "../../../../graphql/generated"
+import { unAuthRedirect } from "../../../../config"
+import { UserTeamNav } from "../../../../components/User/Team/Nav"
+import { useQuery } from "@apollo/client"
+import { useRouter } from "next/router"
+
+const UserTeamsTeamPage: NextPage = () => {
+  const { query } = useRouter()
+  const { data, error } = useQuery<UserTeamsTeamPageQuery>(
+    UserTeamsTeamPageDocument,
+    { variables: { id: query.teamId } }
+  )
+  console.log(data?.team?.members)
+
+  return (
+    <main role="main">
+      <h1>{data?.team?.name || "Team"}</h1>
+      {error ? (
+        <p>Something went wrong...</p>
+      ) : (
+        <>
+          <UserTeamNav />
+          <h3>{data?.team?.name}</h3>
+          <p>{data?.team?.description}</p>
+          <h4>Members</h4>
+          <ul style={{ padding: "0" }}>
+            {data?.team?.members?.map((member) => (
+              <li
+                key={member.id}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "2rem",
+                  borderBottom: "1px solid black",
+                }}
+              >
+                <h5>{member.fullName}</h5>
+                <small>{member.email}</small>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </main>
+  )
+}
+
+export default UserTeamsTeamPage
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const apolloClient = initializeApollo({ headers: context.req.headers })
+
+  const { data } = await apolloClient.query({
+    query: UserTeamsTeamPageDocument,
+    variables: { id: context.query.teamId as never },
+  })
+
+  // check if user is logged in
+  // if not, redirect to login page
+  if (!data.authenticatedItem?.id) return unAuthRedirect
+
+  return addApolloState(apolloClient, {
+    props: {},
+  })
+}
