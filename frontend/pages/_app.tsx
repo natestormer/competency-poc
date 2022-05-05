006aefa14066a16efbfd7c56cea30cdcafe6a84e
natestormer <nate@smartrent.com>
2022-05-05 16:50:57 -0700

    adds progress bar on route change

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from "next/app"
+import Router from "next/router"
 import { ApolloProvider } from "@apollo/client"
+import NProgress from "nprogress"
 
 import { useApollo } from "../lib/apolloClient"
 import { Header } from "../components/Header"
+
+import "../styles/nprogress.css"
+
+Router.events.on("routeChangeStart", () => NProgress.start())
+Router.events.on("routeChangeComplete", () => NProgress.done())
+Router.events.on("routeChangeError", () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps)
