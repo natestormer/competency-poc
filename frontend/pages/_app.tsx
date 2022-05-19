65f63cb31f33e47682ece4dc609091057208f045
natestormer <nate@smartrent.com>
2022-05-19 16:58:11 -0700

    adds styled components

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app"
 import Router from "next/router"
 import { ApolloProvider } from "@apollo/client"
 import NProgress from "nprogress"
+import { createGlobalStyle } from "styled-components"
 
 import { useApollo } from "../lib/apolloClient"
 import { Header } from "../components/Header"
@@ -12,14 +13,30 @@ Router.events.on("routeChangeStart", () => NProgress.start())
 Router.events.on("routeChangeComplete", () => NProgress.done())
 Router.events.on("routeChangeError", () => NProgress.done())
 
+// https://github.com/vercel/next.js/blob/canary/examples/with-styled-components/pages/_app.js
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --primary-rgb-vals: 15, 120, 89;
+    --primary-hex: #0F7859;
+    --primary: rgba(var(--primary-rgb-vals), 1);
+  }
+
+  a {
+    color: var(--primary);
+  }
+`
+
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps)
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <Header />
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <GlobalStyle />
+      <ApolloProvider client={apolloClient}>
+        <Header />
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
   )
 }
 
